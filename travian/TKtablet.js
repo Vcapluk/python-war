e2b6d5d2c92e842b5bc65cba4632cf5895b2f2f3
Vcapluk <Vcapluk@gmail.com>
2023-11-21 18:08:15 +0500

    modified:   travian/TKtablet.js 	modified:   travian/popup.html

--- travian/TKtablet.js
+++ travian/TKtablet.js
@@ -32,9 +32,10 @@ function createTable(parent, cols, rows) {
 // выводит число рандомное.
 function randomInteger(min, max) {
     document.querySelector('.sum').innerHTML = (Math.floor(Math.random() * (max - min + 1) ) + min);
+    document.querySelector('.sumx').innerHTML = (Math.floor(Math.random() * (max - min + 1) ) + min + 10000);
     let currentDate = new Date (); //принимает текущую дату
     console.log (currentDate); //выводит в консоль текущую дату
-    var elC = document.querySelector(".stockContainer.clay .production .value"); //вытащили строку выработки глины
+    /*var elC = document.querySelector(".stockContainer.clay .production .value"); //вытащили строку выработки глины
     const contentC = elC.innerText;// достаем текст из дива
     var xC = parseInt(contentC, 10);// из текста достаем число
     //console.log(xC);//вывели в консоль
@@ -49,9 +50,18 @@ function randomInteger(min, max) {
     //console.log(xCr);//вывели в консоль
     var itogCHVR = xW + xC +xI + xCr;
     console.log("ЧВР на сейчас равно: " + itogCHVR); // получаем ЧВР в консоль
-    var message123 = "ЧВР на сейчас равно: " + itogCHVR;
+    var message123 = "ЧВР на сейчас равно: " + itogCHVR;*/
+    document.querySelector('.sumx').innerHTML = (Math.floor(Math.random() * (max - min + 1) ) + min + 10000);
     //alert(message123);
     //document.writeln(message123);//добавлять не стоит, вместо кнопочек выводит только сообщение...
 }
 
 console.log("последняя строка в последнем скрипте")
+
+
+//https://ru1.kingdoms.com/#/page:village/villId:538132443 01.аааааа
+//https://ru1.kingdoms.com/#/page:village/villId:537214958 Деревня тестя
+
+Village.get(538132443).production
+
+https://d.delivery.consentmanager.net/delivery/cmp.php?id=17155&h=https%3A%2F%2Fru1.kingdoms.com%2F%23%2Fpage%3Avillage%2FvillId%3A538132443&__cmpfcc=1&l=ru&ls=RU_EN_RU&lp=RU&o=1700567312137
