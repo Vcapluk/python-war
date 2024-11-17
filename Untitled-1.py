29bfbd97cd39a787762c236329d6398a4b7924db
vcapluk <vcapluk@gmail.com>
2024-11-17 17:43:18 +0500

    modified:   .vscode/settings.json 	modified:   Untitled-1.py 	new file:   tkinter/__pycache__/tkinter.cpython-312.pyc 	new file:   tkinter/tkinter1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,5 +1,64 @@
-print('Программа, в которой рассчитывается сумма и произведение цифр положительного трёхзначного числа')
-a1 = False
+from cProfile import label
+from email import message
+from tkinter import *
+from tkinter import ttk
+
+
+
+def show_message():
+    label["text"] = entry.get()
+    a = entry.get()
+    message_one(a)
+
+ 
+root = Tk()
+root.title("Vcapluk")
+root.geometry("400x200")
+
+message = StringVar()
+
+label1 = ttk.Label(text='Программа для расчета числа', width=200, justify=CENTER)
+label1.pack()
+
+entry = ttk.Entry(textvariable = message)
+entry.pack(anchor=NW, padx = 6, pady=6)
+
+btn1 = ttk.Button(text="Ok!", command=show_message)
+btn1.pack(side = RIGHT,anchor=NW, padx = 6, pady=6)
+
+label = ttk.Label()
+label.pack(anchor=NW, padx = 6, pady=6)
+
+
+#print('Программа, в которой рассчитывается сумма и произведение цифр положительного трёхзначного числа')
+def message_one(a):
+    print('вошли')
+    a1 = False
+    a999 = 1
+    while a1 == False:
+        try:
+            a = int(input("Введите число: "))
+            a3 = a/100
+            a3 > 1
+            a3 <10
+            a999 = str(a)
+            #print(a3)
+            if len(str(a)) == 3:
+                a2 = str(a)
+                print('Сумма цифр =', int(a2[0]) + int(a2[1]) + int(a2[2]))
+                print('Произведение цифр =', int(a2[0]) * int(a2[1]) * int(a2[2]))
+                a1 = True
+                break
+            else:
+                print('Ошибочка вышла, надо 3-х значное!')
+        except ValueError:
+            print("Это разве число?")
+        else:
+            print('Давайте ещё разик!')
+            #a1 = True
+
+
+'''a1 = False
 a999 = 1
 while a1 == False:
     try:
@@ -49,3 +108,6 @@ f.write(f21)
 f.write('\n\n')
 f.close()
 print('конец...')
+'''
+
+root.mainloop()
---
+++ tkinter/tkinter1.py
@@ -0,0 +1,15 @@
+#https://metanit.com/python/tkinter/
+
+
+from tkinter import *
+from tkinter import ttk
+ 
+root = Tk()
+root.title("METANIT.COM")
+root.geometry("250x200")
+ 
+# стандартная кнопка
+btn = ttk.Button(text="Button")
+btn.pack()
+ 
+root.mainloop()
