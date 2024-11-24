f6b2cfaabe8c95e607227f2b24473169a8e8391e
vcapluk <vcapluk@gmail.com>
2024-11-24 22:25:56 +0500

    modified:   "\321\201\321\202\321\200\320\260\320\275\320\270\321\206\321\213 \321\201 \320\272\320\276\320\264\320\276\320\274/\321\202\320\260\320\261\320\273\320\270\321\207\320\272\320\260 \320\221\320\235-II/script.js" 	modified:   "\321\201\321\202\321\200\320\260\320\275\320\270\321\206\321\213 \321\201 \320\272\320\276\320\264\320\276\320\274/\321\202\320\260\320\261\320\273\320\270\321\207\320\272\320\260 \320\221\320\235-II/style.css" 	modified:   "\321\201\321\202\321\200\320\260\320\275\320\270\321\206\321\213 \321\201 \320\272\320\276\320\264\320\276\320\274/\321\202\320\260\320\261\320\273\320\270\321\207\320\272\320\260 \320\221\320\235-II/tablet.html"

--- страницы с кодом/табличка БН-II/script.js
+++ страницы с кодом/табличка БН-II/script.js
@@ -1,19 +1,201 @@
+
+{
+    "playerId": 1636,
+    "name": "Володька Царь",
+    "tribeId": 3,
+    "kingdomId": 0,
+    "kingdomTag": "",
+    "kingdomRole": 0,
+    "isKing": false,
+    "kingId": 0,
+    "kingstatus": 0,
+    "villages": [
+        {
+            "villageId": "536264685",
+            "playerId": "1636",
+            "name": "New village",
+            "tribeId": "3",
+            "belongsToKing": "0",
+            "belongsToKingdom": 0,
+            "type": "1",
+            "population": "9",
+            "coordinates": {
+                "x": "-19",
+                "y": "-19"
+            },
+            "isMainVillage": true,
+            "isTown": false,
+            "treasuresUsable": "0",
+            "treasures": "0",
+            "protectionGranted": "0",
+            "tributeCollectorPlayerId": 0,
+            "realTributePercent": 0.2,
+            "supplyBuildings": "9",
+            "supplyTroops": "9",
+            "production": {
+                "1": "20",
+                "2": "20",
+                "3": "20",
+                "4": "17"
+            },
+            "storage": {
+                "1": 561.5611111111177,
+                "2": 564.5611111111177,
+                "3": 577.5611111111177,
+                "4": 626.4702777777811
+            },
+            "treasury": {
+                "1": "0",
+                "2": "0",
+                "3": "0",
+                "4": 0
+            },
+            "storageCapacity": {
+                "1": "800",
+                "2": "800",
+                "3": "800",
+                "4": "800"
+            },
+            "usedControlPoints": "0",
+            "availableControlPoints": "0",
+            "culturePoints": 0.027766203703702443,
+            "celebrationType": "0",
+            "celebrationEnd": "0",
+            "culturePointProduction": "12",
+            "treasureResourceBonus": "0",
+            "acceptance": 100,
+            "acceptanceProduction": "0.01666",
+            "healingTentCapacity": "0",
+            "tributes": {
+                "1": 0,
+                "2": 0,
+                "3": 0,
+                "4": 0
+            },
+            "tributesCapacity": "800",
+            "tributeTreasures": 1,
+            "tributeProduction": 12,
+            "tributeProductionWithCrop": 15,
+            "tributeProductionDetail": {
+                "1": 4,
+                "2": 4,
+                "3": 4,
+                "4": 0
+            },
+            "tributeTime": "0",
+            "tributesRequiredToFetch": 160,
+            "estimatedWarehouseLevel": 0,
+            "allowTributeCollection": "1",
+            "tributesDisabledReason": ""
+        }
+    ],
+    "population": 9,
+    "active": 1,
+    "prestige": 11383,
+    "level": 12,
+    "stars": {
+        "goldBadge": 1,
+        "bronze": 0,
+        "silver": 0,
+        "gold": 0
+    },
+    "nextLevelPrestige": 20000,
+    "hasNoobProtection": true,
+    "filterInformation": false,
+    "signupTime": 1732464879,
+    "vacationState": 0,
+    "uiLimitations": 0,
+    "gold": 40,
+    "silver": 0,
+    "deletionTime": 0,
+    "coronationDuration": 0,
+    "brewCelebration": 0,
+    "uiStatus": -1,
+    "hintStatus": 0,
+    "spawnedOnMap": 1732465024,
+    "isActivated": 1,
+    "isInstant": 0,
+    "productionBonusTime": 0,
+    "cropProductionBonusTime": 0,
+    "premiumFeatureAutoExtendFlags": 0,
+    "plusAccountTime": 0,
+    "limitedPremiumFeatureFlags": 0,
+    "lastPaymentTime": 0,
+    "isPunished": false,
+    "limitationFlags": 0,
+    "limitation": 0,
+    "isBannedFromMessaging": false,
+    "bannedFromMessaging": 0,
+    "questVersion": 2,
+    "nextDailyQuestTime": 1732508081,
+    "dailyQuestsExchanged": 0,
+    "avatarIdentifier": 6972875,
+    "vacationStateStart": 0,
+    "vacationStateEnd": 0,
+    "usedVacationDays": 0,
+    "halloweenBoostTime": 0,
+    "mayhemBoostTime": 0,
+    "fealtyPoints": 0,
+    "lastFealtyProduction": 0,
+    "fealtyProductionBonusTime": 0
+}
+
+let spisokBNII_1 = 'БН-II'
 let spisokBNII = [
-    {sekcia: 'Секция №1', yach:'БН-1', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №1', yach:'БН-2', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №2', yach:'БН-3', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №2', yach:'БН-4', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №3', yach:'БН-5', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №3', yach:'БН-6', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №4', yach:'БН-7', zn: true, pz: false, zametki:''},
-    {sekcia: 'Секция №4', yach:'БН-8', zn: true, pz: false, zametki:''},
+
+    {terra: 'БН-II',sekcia: 'Секция №1', yach:'БН-1', ya: true,  zn: true, pz: true, zametki:'ТН'},
+    {terra: 'БН-II',sekcia: 'Секция №1', yach:'БН-2', ya: true, zn: true, pz: false, zametki:'Ввод'},
+    {terra: 'БН-II',sekcia: 'Секция №2', yach:'БН-3', ya: true, zn: false, pz: true, zametki:'масло ф.А'},
+    {terra: 'БН-II',sekcia: 'Секция №2', yach:'БН-4', ya: true, zn: false, pz: false, zametki:'Блокировка'},
+    {terra: 'БН-II',sekcia: 'Секция №3', yach:'БН-5', ya: false, zn: true, pz: true, zametki:''},
+    {terra: 'БН-II',sekcia: 'Секция №3', yach:'БН-6', ya: false, zn: true, pz: false, zametki:''},
+    {terra: 'БН-II',sekcia: 'Секция №4', yach:'БН-7', ya: false, zn: false, pz: true, zametki:''},
+    {terra: 'БН-II',sekcia: 'Секция №4', yach:'БН-8', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-1', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-2', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-3', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-4', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-5', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-6', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-7', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'Золоотвал №2',sekcia: '-', yach:'ВЛ-8', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'ВЛ-1',sekcia: '-', yach:'ЛР-1', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'ВЛ-2',sekcia: '-', yach:'ЛР-2', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'ВЛ-3',sekcia: '-', yach:'ЛР-3', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'ВЛ-4',sekcia: '-', yach:'ЛР-4', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'ВЛ-1',sekcia: '-', yach:'ЛР-5', ya: false, zn: true, pz: false, zametki:''},
+    {terra: 'ВЛ-2',sekcia: '-', yach:'ЛР-6', ya: false, zn: true, pz: false, zametki:''},
+    {terra: 'ВЛ-3',sekcia: '-', yach:'ЛР-7', ya: false, zn: false, pz: false, zametki:''},
+    {terra: 'ВЛ-4',sekcia: '-', yach:'ЛР-8', ya: false, zn: false, pz: false, zametki:''},
+
 ]
 
 let tableSpisokBNII = ''
 
 for (let n = 0; n < spisokBNII.length; n++) {
     let qwer = spisokBNII[n]
-    tableSpisokBNII += '<tr><td>'+qwer.sekcia+'</td><td>'+qwer.yach+'</td><td>'+qwer.zn+'</td><td>'+qwer.pz+'</td><td>'+qwer.zametki+'</td></tr>'
+    let yaqwer = qwer.ya
+    if (qwer.ya === true){
+        yaqwer = '<input type="checkbox" checked name="' + qwer.yach + 'ya"/>'
+        qwer.ya = yaqwer
+    }else{
+        qwer.ya = '<input type="checkbox" name="' + qwer.yach + 'ya"/>'
+    }
+    let znqwer = qwer.zn
+    if (qwer.zn === true){
+        znqwer = '<input type="checkbox" checked name="' + qwer.yach + 'zn"/>'
+        qwer.zn = znqwer
+    }else{
+        qwer.zn = '<input type="checkbox" name="' + qwer.yach + 'zn"/>'
+    }
+    let pzqwer = qwer.pz
+    if (qwer.pz === true){
+        pzqwer = '<input type="checkbox" checked name="' + qwer.yach + 'pz"/>'
+        qwer.pz = pzqwer
+    }else{
+        qwer.pz = '<input type="checkbox" name="' + qwer.yach + 'pz"/>'
+    }
+    tableSpisokBNII += '<tr><td>'+qwer.terra+'</td><td>'+qwer.sekcia+'</td><td>'+qwer.yach+'</td><td>'+qwer.ya+'</td><td>'+qwer.zn+'</td><td>'+qwer.pz+'</td><td>'+qwer.zametki+'</td></tr>'
 
 
 }
@@ -21,9 +203,17 @@ for (let n = 0; n < spisokBNII.length; n++) {
 const div = document.createElement('div');
 
 str = '';
-str += '<li>Строения для наса 500<table>'
+str += '<li>' + spisokBNII_1 + '<table>'
+str += '<tr><td>Размещение</td><td>Секция</td><td>Ячейка</td><td>Полжение</td><td>ЗН</td><td>ПЗ</td><td>Заметки</td></tr>'
 str += tableSpisokBNII
 str += '</table></li>'
+
+
+//создаем контроль клика...
+
+
+
+
 
 div.innerHTML = str;//содержимое селектора диалог
 console.log("придали ей содержимое");
