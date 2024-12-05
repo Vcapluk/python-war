b386aaeb66179164e6440adf97429b765e64bdcd
vcapluk <vcapluk@gmail.com>
2024-12-05 16:32:31 +0500

    modified:   pyQT6/alisa.py 	modified:   pyQT6/date.py 	new file:   "pyQT6/\321\205\321\200\320\265\320\275\321\214.py"

--- pyQT6/alisa.py
+++ pyQT6/alisa.py
@@ -9,17 +9,41 @@ from PyQt6.QtCore import Qt
 
 with open('example.txt', 'rb') as file:
     readed_data = pickle.load(file)
-    print(type(readed_data), readed_data)
+    #print(type(readed_data), readed_data)
 
 
 with open('test1.txt', 'rb') as file:
     readed_data = pickle.load(file)
-    print(type(readed_data), readed_data)
+    #print(type(readed_data), readed_data)
+
+'''
+print('начнем...')
+value = readed_data.get('Присоединение')
+print(value) 
+print(value[0])
+valuelen = len(value)
+print(valuelen)# гуд, выдает количество столбиков
+'''
+qwerty = len(readed_data.get('Присоединение'))
+print(qwerty)
+
+print('А сейчас магия не случится')
+#qwer1 = readed_data
+#print(qwer1)
+keys = list(readed_data.keys())
+keys1 = keys[1]
+print(type(keys1))
+print(keys1)
 
 
+print('А сейчас магия не случится')
+print('А сейчас магия не случится')
+print('А сейчас магия не случится')
+print(type(keys[1]))
+print(keys[1])
 
-
-
+rowfull = int(len(readed_data))
+columnfull = int(len(readed_data.get('Присоединение')))
 class MyTable(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -27,18 +51,48 @@ class MyTable(QtWidgets.QMainWindow):
         self.setCentralWidget(self.table_widget)
 
         # Устанавливаем количество строк и столбцов в таблице
-        self.table_widget.setRowCount(5) # строки
-        self.table_widget.setColumnCount(3) #столбики
+        self.table_widget.setRowCount(rowfull) # строки
+        self.table_widget.setColumnCount(columnfull) #столбики
+        keys = list(readed_data.keys())# говорим пролистать все значения ключей и записать их в список
 
         # Создаём цикл для добавления чекбоксов в каждую ячейку таблицы
         for row in range(self.table_widget.rowCount()):# в каждую строку
+            keysnow = keys[row] #говорю, какую строку списка будем смотреть
+            print('------ новые значения ------')
+            print(keysnow)
             for column in range(self.table_widget.columnCount()):# для каждого столбца
-                checkbox_item = QtWidgets.QTableWidgetItem(str(row))#
+                qwer = readed_data.get(keysnow)
+                value = qwer[column] # предполагаю, что должен смотреть на содержание списка и вставлять его в нужную ячейку
+                
+                #print(value)
+                #checkbox_item = QtWidgets.QTableWidgetItem(str(row))#пока не понятно...
+                checkbox_item = QtWidgets.QTableWidgetItem()#пока не понятно...
+                #print(value)
+                # здесь проверяем содержимое. если тру или фолс - то чекбокс актив/неактив, иначе просто текст
+                if value == True:
+                    checkbox_item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
+                    checkbox_item.setCheckState(Qt.CheckState.Checked)
+                    #checkbox_item.setText('включен')
+                    self.table_widget.setItem(row, column, checkbox_item)
+                elif value == False:
+                    checkbox_item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
+                    checkbox_item.setCheckState(Qt.CheckState.Unchecked)
+                    #checkbox_item.setText('выключен')
+                    self.table_widget.setItem(row, column, checkbox_item)
+                else:
+                    checkbox_item.setText(value)
+                    self.table_widget.setItem(row, column, checkbox_item)
+                    #print(value)
+
+
+'''
                 if column == 1:#столбиков 3. 0, 1, 2. Здесь во второй столбик добавляем чекбоксы
                     checkbox_item.setFlags(Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEnabled)
                     checkbox_item.setCheckState(Qt.CheckState.Unchecked)
                     checkbox_item.setText('текст  у чекбокса')
                     self.table_widget.setItem(row, column, checkbox_item)
+'''
+
 
 def eventFilter(self, source, event):
     print(event)
@@ -49,6 +103,6 @@ if __name__ == "__main__":
     # Создание окна
     window = MyTable()
     # Отобразить окно на экране
-    window.resize(330, 200)
+    window.resize(800, 900)
     window.show()
     sys.exit(app.exec())
